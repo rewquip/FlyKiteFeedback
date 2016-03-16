b4cd113d0621f730b7fb5f1dc4da2239b8d6318b
rewquip <imustnotbeknown@hotmail.com>
2016-03-16 23:31:59 +0800

    database name
    darrenlim2012

--- index.php
+++ index.php
@@ -2,14 +2,14 @@
 <body>
 <?php
 $serverName = "http://darrenlim2012.database.windows.net";
-$connectionInfo = array("Database"=>"FlyKiteFeedback", "UID"=>"darren.lim.2012", "PWD"=>"Password1");
-$conn = sqlsrv_connect( $serverName, $connectionInfo);
+$connectionInfo = array("Database"=>"darrenlim2012", "UID"=>"darren.lim.2012", "PWD"=>"Password1");
+$conn = sqlsrv_connect($serverName, $connectionInfo);
 
-if($conn ) {
+if($conn) {
 	echo "Connection established.<br/>";
-}else{
+} else {
 	echo "Connection could not be established.<br/>";
-	die(print_r( sqlsrv_errors(), true));
+	die(print_r(sqlsrv_errors(), true));
 }
 
 echo "My first PHP script!";
