c9b76aee2e259dfc4f3405198395702edf09299b
rewquip <imustnotbeknown@hotmail.com>
2016-03-17 00:01:15 +0800

    sqlsvr
    connect

--- index.php
+++ index.php
@@ -1,20 +1,22 @@
 <html>
 <body>
-<?php
-  try {
-    $hostname = "http://darrenlim2012.database.windows.net";
-    $port = 1433;
-    $dbname = "FlyKiteFeedback";
-    $username = "darren.lim.2012";
-    $pw = "Password1";
-    $dbh = new PDO ("dblib:host=$hostname:$port;dbname=$dbname","$username","$pw");
-  } catch (PDOException $e) {
-    echo "Failed to get DB handle: " . $e->getMessage() . "\n";
-    exit;
-  }
-  
-  echo "My first PHP script!";
-?>
+	<?php
+		$serverName = "tcp:darrenlim2012.database.windows.net,1433";
+		$userName = 'darren.lim.2012@darrenlim2012';
+		$userPassword = 'Password1';
+		$dbName = "FlyKiteFeedback";
+		$table = "test";
 
+		$connectionInfo = array("Database"=>$dbName, "UID"=>$userName, "PWD"=>$userPassword, "MultipleActiveResultSets"=>true);
+
+		sqlsrv_configure('WarningsReturnAsErrors', 0);
+		$conn = sqlsrv_connect($serverName, $connectionInfo);
+		if($conn === false)
+		{
+		 FatalError("Failed to connect...");
+		}
+
+		echo "My first PHP script!";
+	?>
 </body>
 </html>
