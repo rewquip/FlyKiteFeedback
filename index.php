06b6a99c9e42097aaabc2969d1fbe6993090e3d5
rewquip <imustnotbeknown@hotmail.com>
2016-03-17 00:07:27 +0800

    double quotes
    <p>

--- index.php
+++ index.php
@@ -2,19 +2,19 @@
 <body>
 	<?php
 		$serverName = "tcp:darrenlim2012.database.windows.net,1433";
-		$userName = 'darren.lim.2012';
-		$userPassword = 'Password1';
+		$userName = "darren.lim.2012";
+		$userPassword = "Password1";
 		$dbName = "FlyKiteFeedback";
 		$table = "test";
-
+		
 		$connectionInfo = array("Database"=>$dbName, "UID"=>$userName, "PWD"=>$userPassword, "MultipleActiveResultSets"=>true);
-
-		sqlsrv_configure('WarningsReturnAsErrors', 0);
+		sqlsrv_configure("WarningsReturnAsErrors", 0);
 		$conn = sqlsrv_connect($serverName, $connectionInfo);
+		
 		if($conn === false) {
 			FatalError("Failed to connect...");
 		} else {
-			echo "yay";
+			echo "Connected<p>";
 		}
 
 		echo "My first PHP script!";
