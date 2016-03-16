86399e0fe9b154e728c06524d72080d430f01cda
rewquip <imustnotbeknown@hotmail.com>
2016-03-17 01:37:47 +0800

    rating
    comment

--- index.php
+++ index.php
@@ -1,26 +1,12 @@
 <html>
-<body>
-	<?php
-		$serverName = "tcp:darrenlim2012.database.windows.net,1433";
-		$dbName = "FlyKiteFeedback";
-		$userName = "darren.lim.2012";
-		$userPassword = "Password1";
-		$connectionInfo = array("Database"=>$dbName, "UID"=>$userName, "PWD"=>$userPassword);
-		$conn = sqlsrv_connect($serverName, $connectionInfo);
-		
-		$sql = "insert into test (ID, Name) values (?, ?)";
-		$params = array(7, "Darren7");
-		$stmt = sqlsrv_query($conn, $sql, $params);
-		
-		$comment = $gender = "";
-	?>
-	
+<body>	
 	<form method="post" action="success.php"> 
-		Comment: <textarea name="comment" rows="5" cols="40"></textarea>
+		Rating:
+		<input type="radio" name="rating" value="good">Good
+		<input type="radio" name="rating" value="bad">Bad
 		<br><br>
-		Gender:
-		<input type="radio" name="gender" value="female">Female
-		<input type="radio" name="gender" value="male">Male
+		Comment:
+		<textarea name="comment" rows="5" cols="40" maxlength="255"></textarea>
 		<br><br>
 		<input type="submit" name="submit" value="Submit"> 
 	</form>
--- success.php
+++ success.php
@@ -1,8 +1,19 @@
 <html>
 <body>
 	<?php
-		echo $_POST["comment"];
-		echo $_POST["gender"];
+		$serverName = "tcp:darrenlim2012.database.windows.net,1433";
+		$dbName = "FlyKiteFeedback";
+		$userName = "darren.lim.2012";
+		$userPassword = "Password1";
+		$connectionInfo = array("Database"=>$dbName, "UID"=>$userName, "PWD"=>$userPassword);
+		$conn = sqlsrv_connect($serverName, $connectionInfo);
+		
+		$rating = $_POST["rating"];
+		$comment = $_POST["comment"];
+		
+		$sql = "insert into test (ID, Name) values (?, ?)";
+		$params = array($rating, $comment);
+		$stmt = sqlsrv_query($conn, $sql, $params);
 	?>
 </body>
 </html>
