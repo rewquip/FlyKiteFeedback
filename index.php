516517d1123080a9a8d9c8ccfc5bf300dc8772e9
rewquip <imustnotbeknown@hotmail.com>
2016-03-16 23:19:12 +0800

    sql connection
    connect

--- index.php
+++ index.php
@@ -1,10 +1,18 @@
-<!DOCTYPE html>
 <html>
 <body>
-
 <?php
+$serverName = "http://darrenlim2012.database.windows.net";
+$connectionInfo = array("Database"=>"FlyKiteFeedback", "UID"=>"darren.lim.2012", "PWD"=>"Password1");
+$conn = sqlsrv_connect( $serverName, $connectionInfo);
+
+if($conn ) {
+	echo "Connection established.<br/>";
+}else{
+	echo "Connection could not be established.<br/>";
+	die(print_r( sqlsrv_errors(), true));
+}
+
 echo "My first PHP script!";
 ?>
-
 </body>
 </html>
