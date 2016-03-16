36925d1ce6feca4761aee818963e1e0ca40c8bc0
rewquip <imustnotbeknown@hotmail.com>
2016-03-17 00:36:06 +0800

    compact
    7

--- index.php
+++ index.php
@@ -5,30 +5,12 @@
 		$userName = "darren.lim.2012";
 		$userPassword = "Password1";
 		$dbName = "FlyKiteFeedback";
-		
-		$connectionInfo = array("Database"=>$dbName, "UID"=>$userName, "PWD"=>$userPassword, "MultipleActiveResultSets"=>true);
-		sqlsrv_configure("WarningsReturnAsErrors", 0);
+		$connectionInfo = array("Database"=>$dbName, "UID"=>$userName, "PWD"=>$userPassword);
 		$conn = sqlsrv_connect($serverName, $connectionInfo);
 		
-		if($conn === false) {
-			FatalError("Failed to connect...");
-		}
-		
 		$sql = "insert into test (ID, Name) values (?, ?)";
-		$params = array(5, "Darren5");
+		$params = array(7, "Darren7");
 		$stmt = sqlsrv_query($conn, $sql, $params);
-		
-		/*
-		$sql = "SELECT * from test";
-		$stmt = sqlsrv_query($conn, $sql);
-		while($obj = sqlsrv_fetch_object($stmt))
-			echo $obj->ID.", ".$obj->Name."<br/>";
-		*/
-		
-		echo "<p>My first PHP script!";
 	?>
-	<script>
-		console.log(<? echo json_encode($conn); ?>);
-	</script>
 </body>
 </html>
