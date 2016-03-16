237076d9de8e90ec7caaf629ed16183adce5fcd5
rewquip <imustnotbeknown@hotmail.com>
2016-03-17 01:49:42 +0800

    bootstrap
    drahpbahx

--- index.php
+++ index.php
@@ -1,4 +1,9 @@
 <html>
+<head>
+	<title>FlyKite! Feedback Portal</title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+</head>
 <body>	
 	<form method="post" action="success.php"> 
 		Rating:
--- success.php
+++ success.php
@@ -1,4 +1,9 @@
 <html>
+<head>
+	<title>FlyKite! Feedback Portal</title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+</head>
 <body>
 	<?php
 		$serverName = "tcp:darrenlim2012.database.windows.net,1433";
