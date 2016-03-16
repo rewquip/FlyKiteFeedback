b127de345e967a86cae3425d4297ea2a8ab3cebf
rewquip <imustnotbeknown@hotmail.com>
2016-03-17 01:31:22 +0800

    forgot
    no validation

--- index.php
+++ index.php
@@ -13,37 +13,14 @@
 		$stmt = sqlsrv_query($conn, $sql, $params);
 		
 		$comment = $gender = "";
-		
-		if ($_SERVER["REQUEST_METHOD"] == "POST") {
-			$valid = true;
-			
-			if (empty($_POST["comment"])) {
-				$comment = "";
-			} else {
-				$comment = $_POST["comment"];
-			}
-			
-			if (empty($_POST["gender"])) {
-				$genderErr = "Gender is required";
-				$valid = false;
-			} else {
-				$gender = $_POST["gender"];
-			}
-			
-			if($valid){
-				header("location:success.php");
-				exit();
-			}
-		}
 	?>
 	
-	<form method="post" action="<?php echo $_SERVER["PHP_SELF"]; ?>"> 
-		Comment: <textarea name="comment" rows="5" cols="40"><?php echo $comment;?></textarea>
+	<form method="post" action="success.php"> 
+		Comment: <textarea name="comment" rows="5" cols="40"></textarea>
 		<br><br>
 		Gender:
-		<input type="radio" name="gender" <?php if (isset($gender) && $gender=="female") echo "checked";?> value="female">Female
-		<input type="radio" name="gender" <?php if (isset($gender) && $gender=="male") echo "checked";?> value="male">Male
-		<span class="error">* <?php echo $genderErr; ?></span>
+		<input type="radio" name="gender" value="female">Female
+		<input type="radio" name="gender" value="male">Male
 		<br><br>
 		<input type="submit" name="submit" value="Submit"> 
 	</form>
