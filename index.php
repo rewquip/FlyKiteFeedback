dbc408542baa32e29ddb4037a50c539212822079
rewquip <imustnotbeknown@hotmail.com>
2016-03-18 01:11:29 +0800

    margin bottom
    40

--- index.php
+++ index.php
@@ -15,10 +15,10 @@
 	<div class="col-md-8 col-md-offset-2">
 		<div class="page-header text-center">
 			<div class="row">
-				<div class="col-md-2 col-md-offset-1">
+				<div class="col-md-3" style="margin-left: 30;">
 					<img src="logo.png">
 				</div>
-				<div class="col-md-8">
+				<div class="col-md-8" style="padding-top: 15;">
 					<h1>FlyKite! Feedback Portal</h1>
 					<small>We value your feedback greatly and welcome any comments you may have to help improve our programs and services.</small><p>
 				</div>
@@ -201,7 +201,7 @@
 				<label>Do you have any suggestions for FlyKite's improvement?</label><br>
 				<textarea class="form-control" name="comments" rows="5" maxlength="3000"></textarea>
 			</div>
-			<div class="form-group text-center">
+			<div class="form-group text-center" style="margin-bottom: 40">
 				<button class="btn btn-default" type="submit">Submit
 			</div>
 		</form>
