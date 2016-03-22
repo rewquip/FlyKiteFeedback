b8d2079fa61a435b394b431265b194d94ef7e330
rewquip <imustnotbeknown@hotmail.com>
2016-03-22 15:28:14 +0800

    v-align
    table

--- index.php
+++ index.php
@@ -15,193 +15,179 @@
 	<div class="col-md-8 col-md-offset-2">
 		<div class="page-header text-center">
 			<div class="row">
-				<div class="col-md-3" style="margin-left: 30;">
-					<img src="logo.png">
+				<div class="col-md-4" style="padding: 0; display: table-cell; vertical-align: middle; float: none;">
+					<img src="logo.png" style="width: 100%;">
 				</div>
-				<div class="col-md-8" style="padding-top: 15;">
+				<div class="col-md-8" style="display: table-cell; vertical-align: middle; float: none;">
 					<h1>FlyKite! Feedback Portal</h1>
 					<small>We value your feedback greatly and welcome any comments you may have to help improve our programs and services.</small><p>
 				</div>
 			</div>
 		</div>
 		<form method="post" action="success.php">
-			<div class="form-group">
-				<div class="row">
-					<div class="col-md-2">
-					</div>
-					<div class="col-md-2 text-center">
-						<label>Poor</label>
-					</div>
-					<div class="col-md-2 text-center">
-						<label>Fair</label>
-					</div>
-					<div class="col-md-2 text-center">
-						<label>Average</label>
-					</div>
-					<div class="col-md-2 text-center">
-						<label>Good</label>
-					</div>
-					<div class="col-md-2 text-center">
-						<label>Excellent</label>
-					</div>
+			<div class="form-group row">
+				<div class="col-md-2">
+				</div>
+				<div class="col-md-2 text-center">
+					<label>Poor</label>
+				</div>
+				<div class="col-md-2 text-center">
+					<label>Fair</label>
+				</div>
+				<div class="col-md-2 text-center">
+					<label>Average</label>
+				</div>
+				<div class="col-md-2 text-center">
+					<label>Good</label>
+				</div>
+				<div class="col-md-2 text-center">
+					<label>Excellent</label>
 				</div>
 			</div>
-			<div class="form-group">
-				<div class="row">
-					<div class="col-md-2">
-						<label>How would you rate FlyKite's quality of food?</label>
-					</div>
-					<div class="col-md-2 text-center">
-						<label class="radio-inline">
-							<input type="radio" name="food" value="1 - Poor">
-						</label>
-					</div>
-					<div class="col-md-2 text-center">
-						<label class="radio-inline">
-							<input type="radio" name="food" value="2 - Fair">
-						</label>
-					</div>
-					<div class="col-md-2 text-center">
-						<label class="radio-inline">
-							<input type="radio" name="food" value="3 - Average">
-						</label>
-					</div>
-					<div class="col-md-2 text-center">
-						<label class="radio-inline">
-							<input type="radio" name="food" value="4 - Good">
-						</label>
-					</div>
-					<div class="col-md-2 text-center">
-						<label class="radio-inline">
-							<input type="radio" name="food" value="5 - Excellent">
-						</label>
-					</div>
+			<div class="form-group row">
+				<div class="col-md-2" style="display: table-cell; vertical-align: middle; float: none;">
+					<label>How would you rate FlyKite's quality of food?</label>
+				</div>
+				<div class="col-md-2 text-center" style="display: table-cell; vertical-align: middle; float: none;">
+					<label>
+						<input type="radio" name="food" value="1 - Poor">
+					</label>
+				</div>
+				<div class="col-md-2 text-center" style="display: table-cell; vertical-align: middle; float: none;">
+					<label>
+						<input type="radio" name="food" value="2 - Fair">
+					</label>
+				</div>
+				<div class="col-md-2 text-center" style="display: table-cell; vertical-align: middle; float: none;">
+					<label>
+						<input type="radio" name="food" value="3 - Average">
+					</label>
+				</div>
+				<div class="col-md-2 text-center" style="display: table-cell; vertical-align: middle; float: none;">
+					<label>
+						<input type="radio" name="food" value="4 - Good">
+					</label>
+				</div>
+				<div class="col-md-2 text-center" style="display: table-cell; vertical-align: middle; float: none;">
+					<label>
+						<input type="radio" name="food" value="5 - Excellent">
+					</label>
 				</div>
 			</div>
-			<div class="form-group">
-				<div class="row">
-					<div class="col-md-2">
-						<label>How would you rate FlyKite's quality of service?</label>
-					</div>
-					<div class="col-md-2 text-center">
-						<label class="radio-inline">
-							<input type="radio" name="service" value="1 - Poor">
-						</label>
-					</div>
-					<div class="col-md-2 text-center">
-						<label class="radio-inline">
-							<input type="radio" name="service" value="2 - Fair">
-						</label>
-					</div>
-					<div class="col-md-2 text-center">
-						<label class="radio-inline">
-							<input type="radio" name="service" value="3 - Average">
-						</label>
-					</div>
-					<div class="col-md-2 text-center">
-						<label class="radio-inline">
-							<input type="radio" name="service" value="4 - Good">
-						</label>
-					</div>
-					<div class="col-md-2 text-center">
-						<label class="radio-inline">
-							<input type="radio" name="service" value="5 - Excellent">
-						</label>
-					</div>
+			<div class="form-group row">
+				<div class="col-md-2" style="display: table-cell; vertical-align: middle; float: none;">
+					<label>How would you rate FlyKite's quality of service?</label>
+				</div>
+				<div class="col-md-2 text-center" style="display: table-cell; vertical-align: middle; float: none;">
+					<label>
+						<input type="radio" name="service" value="1 - Poor">
+					</label>
+				</div>
+				<div class="col-md-2 text-center" style="display: table-cell; vertical-align: middle; float: none;">
+					<label>
+						<input type="radio" name="service" value="2 - Fair">
+					</label>
+				</div>
+				<div class="col-md-2 text-center" style="display: table-cell; vertical-align: middle; float: none;">
+					<label>
+						<input type="radio" name="service" value="3 - Average">
+					</label>
+				</div>
+				<div class="col-md-2 text-center" style="display: table-cell; vertical-align: middle; float: none;">
+					<label>
+						<input type="radio" name="service" value="4 - Good">
+					</label>
+				</div>
+				<div class="col-md-2 text-center" style="display: table-cell; vertical-align: middle; float: none;">
+					<label>
+						<input type="radio" name="service" value="5 - Excellent">
+					</label>
 				</div>
 			</div>
-			
-			<div class="form-group">
-				<div class="row">
-					<div class="col-md-2">
-					</div>
-					<div class="col-md-2 text-center">
-						<label>Strongly Disagree</label>
-					</div>
-					<div class="col-md-2 text-center">
-						<label>Disagree</label>
-					</div>
-					<div class="col-md-2 text-center">
-						<label>Neutral</label>
-					</div>
-					<div class="col-md-2 text-center">
-						<label>Agree</label>
-					</div>
-					<div class="col-md-2 text-center">
-						<label>Strongly Agree</label>
-					</div>
+			<div class="form-group row">
+				<div class="col-md-2" style="display: table-cell; vertical-align: middle; float: none;">
+				</div>
+				<div class="col-md-2 text-center" style="display: table-cell; vertical-align: middle; float: none;">
+					<label>Strongly Disagree</label>
+				</div>
+				<div class="col-md-2 text-center" style="display: table-cell; vertical-align: middle; float: none;">
+					<label>Disagree</label>
+				</div>
+				<div class="col-md-2 text-center" style="display: table-cell; vertical-align: middle; float: none;">
+					<label>Neutral</label>
+				</div>
+				<div class="col-md-2 text-center" style="display: table-cell; vertical-align: middle; float: none;">
+					<label>Agree</label>
+				</div>
+				<div class="col-md-2 text-center" style="display: table-cell; vertical-align: middle; float: none;">
+					<label>Strongly Agree</label>
 				</div>
 			</div>
-			<div class="form-group">
-				<div class="row">
-					<div class="col-md-2">
-						<label>FlyKite's food has good value for money.</label>
-					</div>
-					<div class="col-md-2 text-center">
-						<label class="radio-inline">
-							<input type="radio" name="money" value="1 - Strongly Disagree">
-						</label>
-					</div>
-					<div class="col-md-2 text-center">
-						<label class="radio-inline">
-							<input type="radio" name="money" value="2 - Disagree">
-						</label>
-					</div>
-					<div class="col-md-2 text-center">
-						<label class="radio-inline">
-							<input type="radio" name="money" value="3 - Neutral">
-						</label>
-					</div>
-					<div class="col-md-2 text-center">
-						<label class="radio-inline">
-							<input type="radio" name="money" value="4 - Agree">
-						</label>
-					</div>
-					<div class="col-md-2 text-center">
-						<label class="radio-inline">
-							<input type="radio" name="money" value="5 - Strongly Agree">
-						</label>
-					</div>
+			<div class="form-group row">
+				<div class="col-md-2" style="display: table-cell; vertical-align: middle; float: none;">
+					<label>FlyKite's food has good value for money.</label>
+				</div>
+				<div class="col-md-2 text-center" style="display: table-cell; vertical-align: middle; float: none;">
+					<label>
+						<input type="radio" name="money" value="1 - Strongly Disagree">
+					</label>
+				</div>
+				<div class="col-md-2 text-center" style="display: table-cell; vertical-align: middle; float: none;">
+					<label>
+						<input type="radio" name="money" value="2 - Disagree">
+					</label>
+				</div>
+				<div class="col-md-2 text-center" style="display: table-cell; vertical-align: middle; float: none;">
+					<label>
+						<input type="radio" name="money" value="3 - Neutral">
+					</label>
+				</div>
+				<div class="col-md-2 text-center" style="display: table-cell; vertical-align: middle; float: none;">
+					<label>
+						<input type="radio" name="money" value="4 - Agree">
+					</label>
+				</div>
+				<div class="col-md-2 text-center" style="display: table-cell; vertical-align: middle; float: none;">
+					<label>
+						<input type="radio" name="money" value="5 - Strongly Agree">
+					</label>
 				</div>
 			</div>
-			<div class="form-group">
-				<div class="row">
-					<div class="col-md-2">
-						<label>I would recommend FlyKite! to others.</label>
-					</div>
-					<div class="col-md-2 text-center">
-						<label class="radio-inline">
-							<input type="radio" name="recommend" value="1 - Strongly Disagree">
-						</label>
-					</div>
-					<div class="col-md-2 text-center">
-						<label class="radio-inline">
-							<input type="radio" name="recommend" value="2 - Disagree">
-						</label>
-					</div>
-					<div class="col-md-2 text-center">
-						<label class="radio-inline">
-							<input type="radio" name="recommend" value="3 - Neutral">
-						</label>
-					</div>
-					<div class="col-md-2 text-center">
-						<label class="radio-inline">
-							<input type="radio" name="recommend" value="4 - Agree">
-						</label>
-					</div>
-					<div class="col-md-2 text-center">
-						<label class="radio-inline">
-							<input type="radio" name="recommend" value="5 - Strongly Agree">
-						</label>
-					</div>
+			<div class="form-group row">
+				<div class="col-md-2" style="display: table-cell; vertical-align: middle; float: none;">
+					<label>I would recommend FlyKite! to others.</label>
+				</div>
+				<div class="col-md-2 text-center" style="display: table-cell; vertical-align: middle; float: none;">
+					<label>
+						<input type="radio" name="recommend" value="1 - Strongly Disagree">
+					</label>
+				</div>
+				<div class="col-md-2 text-center" style="display: table-cell; vertical-align: middle; float: none;">
+					<label>
+						<input type="radio" name="recommend" value="2 - Disagree">
+					</label>
+				</div>
+				<div class="col-md-2 text-center" style="display: table-cell; vertical-align: middle; float: none;">
+					<label>
+						<input type="radio" name="recommend" value="3 - Neutral">
+					</label>
+				</div>
+				<div class="col-md-2 text-center" style="display: table-cell; vertical-align: middle; float: none;">
+					<label>
+						<input type="radio" name="recommend" value="4 - Agree">
+					</label>
+				</div>
+				<div class="col-md-2 text-center" style="display: table-cell; vertical-align: middle; float: none;">
+					<label>
+						<input type="radio" name="recommend" value="5 - Strongly Agree">
+					</label>
 				</div>
 			</div>
-			
-			<div class="form-group">
-				<label>Do you have any suggestions for FlyKite's improvement?</label><br>
+			<div class="form-group" style="margin-top: 20">
+				<label>Do you have any suggestions for FlyKite's improvement?</label>
 				<textarea class="form-control" name="comments" rows="5" maxlength="3000"></textarea>
 			</div>
-			<div class="form-group text-center" style="margin-bottom: 40">
+			<div class="form-group text-center" style="margin-bottom: 50">
 				<button class="btn btn-default" type="submit">Submit
 			</div>
 		</form>
--- success.php
+++ success.php
@@ -7,24 +7,6 @@
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
 </head>
 <body>
-	<?php
-		$serverName = "tcp:darrenlim2012.database.windows.net,1433";
-		$dbName = "FlyKiteFeedback";
-		$userName = "darren.lim.2012";
-		$userPassword = "Password1";
-		$connectionInfo = array("Database"=>$dbName, "UID"=>$userName, "PWD"=>$userPassword);
-		$conn = sqlsrv_connect($serverName, $connectionInfo);
-		
-		$food = $_POST["food"];
-		$service = $_POST["service"];
-		$money = $_POST["money"];
-		$recommend = $_POST["recommend"];
-		$comments = $_POST["comments"];
-		
-		$sql = "insert into Feedback values (?, ?, ?, ?, ?)";
-		$params = array($food, $service, $money, $recommend, $comments);
-		$stmt = sqlsrv_query($conn, $sql, $params);
-	?>
 	<div class="text-center" style="margin-top: 190">
 		<img src="logo.png">
 		<h1>Thank You!</h1><p>
